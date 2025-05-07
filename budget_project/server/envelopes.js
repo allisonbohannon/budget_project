8d17f0a7d91bd3bce5a1a8968254f4af2862d5ad
Allison Bohannon <akboh.github@gmail.com>
2025-05-07 13:23:10 -0700

    Add path for single envelope

--- budget_project/server/envelopes.js
+++ budget_project/server/envelopes.js
@@ -25,11 +25,26 @@ const checkInputs = (req, res, next) => {
     }
 }
 
+//Get all envelopes
 envelopesRouter.get('', (req, res, next) => {
     res.send(envelopes)
 })
+
+//Get envelope by ID
+envelopesRouter.get('/:envelopeId', (req, res, next) => {
+    const envelope = envelopes.find(item => item.id === Number(req.params.envelopeId))
+    res.send(envelope)
+})
+
+//Create new envelope
 envelopesRouter.post('', checkInputs, (req, res, next) => {
-    res.send(req.body)
+    const newEnvelope = {
+        id: envelopes.length + 2, 
+        title: req.body.title, 
+        budget: req.body.budget
+    } 
+    envelopes.push(newEnvelope)
+    res.status(201).send(newEnvelope)
 })
 
 module.exports = envelopesRouter; 
